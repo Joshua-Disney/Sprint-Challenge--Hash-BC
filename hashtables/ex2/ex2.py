54774496b352531a65988867b3835c918fa4a76d
joshua-disney <joshua.a.disney@gmail.com>
2019-10-04 16:13:47 -0700

    Complete ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,11 +14,17 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
     answer = []
 
     for i in range(0, length):
         hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
         print(f"inserted key: {tickets[i].source} value: {tickets[i].destination} into hashtable")
+    
+    while len(answer) < length:
+        if len(answer) < 1:
+            answer.append(hash_table_retrieve(hashtable, "NONE"))
+        else:
+            answer.append(hash_table_retrieve(hashtable, answer[-1]))
+    print(f"answer: {answer}")
 
-    return route
+    return answer
